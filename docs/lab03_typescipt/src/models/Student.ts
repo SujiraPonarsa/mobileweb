307fc1e1176ff2a3811131a7f3f7dd25d0d15a79
“Sujira <sujira.po@kkumail.com>
2025-12-10 23:59:55 +0700

    lab03

---
+++ docs/lab03_typescipt/src/models/Student.ts
@@ -0,0 +1,10 @@
+export interface Student {
+  length: any;
+  id: string;
+  title_name: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  year: number;
+  major: string;
+} 

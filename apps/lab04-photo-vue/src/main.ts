b15f5b1affa4303da2c9286207bcf34ee5b72bd1
SujiraPoronsa <sujira.po@kkumail.com>
2026-01-15 22:19:54 +0700

    Lab4

---
+++ apps/lab04-photo-vue/src/main.ts
@@ -0,0 +1,42 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+
+import { IonicVue } from '@ionic/vue';
+
+// --- เพิ่มส่วนนี้: นำเข้า Element Loader ---
+import { defineCustomElements } from '@ionic/pwa-elements/loader';
+defineCustomElements(window);
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css';
+import '@ionic/vue/css/structure.css';
+import '@ionic/vue/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css';
+import '@ionic/vue/css/float-elements.css';
+import '@ionic/vue/css/text-alignment.css';
+import '@ionic/vue/css/text-transformation.css';
+import '@ionic/vue/css/flex-utils.css';
+import '@ionic/vue/css/display.css';
+
+/* Ionic Dark Mode */
+import '@ionic/vue/css/palettes/dark.system.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+// --- เพิ่มส่วนนี้: เรียกใช้ Loader ก่อน createApp ---
+defineCustomElements(window);
+
+const app = createApp(App)
+  .use(IonicVue)
+  .use(router);
+
+router.isReady().then(() => {
+  app.mount('#app');
+});
---
+++ apps/lab04-photo-vue/src/views/Tab2Page.vue
@@ -0,0 +1,55 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Photo Gallery</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Photo Gallery</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <div style="padding: 10px 15px;">
+        <p>โดย สุจิรา พลอาษา รหัส 663380408-1</p>
+      </div>
+
+      <ion-grid>
+        <ion-row>
+          <ion-col size="6" v-for="photo in photos" :key="photo.filepath">
+            <ion-img :src="photo.webviewPath"></ion-img>
+          </ion-col>
+        </ion-row>
+      </ion-grid>
+
+      <ion-fab vertical="bottom" horizontal="center" slot="fixed">
+        <ion-fab-button @click="addNewToGallery()">
+          <ion-icon :icon="camera"></ion-icon>
+        </ion-fab-button>
+      </ion-fab>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'; // ต้อง import onMounted
+import { 
+  IonPage, IonHeader, IonFab, IonFabButton, 
+  IonIcon, IonToolbar, IonTitle, IonContent,
+  // import สำหรับ Grid
+  IonGrid, IonRow, IonCol, IonImg 
+} from '@ionic/vue';
+import { camera } from 'ionicons/icons';
+import { usePhotoGallery } from '@/composables/usePhotoGallery';
+
+// ดึง photos, addNewToGallery และ loadSaved ออกมาใช้งาน
+const { photos, addNewToGallery, loadSaved } = usePhotoGallery();
+
+
+onMounted(() => {
+  loadSaved();
+});
+</script>
